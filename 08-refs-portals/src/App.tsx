fbc9514c86526f8afba621b01a1a30cf7f27c6e0
kjs <macho199@gmail.com>
2024-01-12 19:00:22 +0900

    init 08-refs-portals prj

---
+++ 08-refs-portals/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Player from "./components/Player";
+
+const App = () => {
+  return (
+    <>
+      <Player />
+      <div id="challenges"></div>
+    </>
+  );
+};
+
+export default App;
