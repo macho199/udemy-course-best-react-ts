9f59c8070cee1d47f1a0bb5c10f30fc1c1cec5b3
kjs <macho199@gmail.com>
2024-01-12 21:40:34 +0900

    add

--- 08-refs-portals/src/components/TimerChallenge.tsx
+++ 08-refs-portals/src/components/TimerChallenge.tsx
@@ -1,19 +1,20 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 type TimerChallengeProps = {
   title: string
   targetTime: number
 };
 
-let timer: NodeJS.Timeout;
+//let timer: NodeJS.Timeout;
 
 const TimerChallenge = ({ title, targetTime }: TimerChallengeProps) => {
+  const timer = useRef<NodeJS.Timeout>(setTimeout(()=>{}));
+
   const [timerStarted, setTimerStarted] = useState<boolean>(false)
   const [timerExpired, setTimerExpired] = useState<boolean>(false)
   
   const handleStart = () => {
-    console.log('handleStart')
-    timer = setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true)
     }, targetTime * 1000);
     
@@ -21,8 +22,7 @@ const TimerChallenge = ({ title, targetTime }: TimerChallengeProps) => {
   }
 
   const handleStop = () => {
-    console.log('handleStop')
-    clearTimeout(timer)
+    clearTimeout(timer.current)
   }
 
   return (
