1820bfc29dc74aafea873a3cdf82ad5b1cd7090b
kjs <macho199@gmail.com>
2024-01-12 19:44:07 +0900

    add

--- 08-refs-portals/src/components/Player.tsx
+++ 08-refs-portals/src/components/Player.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+
+type Name = {
+  name: string
+  inputName: string
+}
 
 const Player = () => {
+  const playerName = useRef<HTMLInputElement>(null);
+  const [enteredPlayerName, setEnteredPlayerName] = useState<string>('');
+
+  const handleClick = () => {
+    if (!playerName.current) return
+    setEnteredPlayerName(playerName.current.value)
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName} entity</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
