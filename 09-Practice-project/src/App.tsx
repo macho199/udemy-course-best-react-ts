ea36b88fcf550eae3a431a436b8c05f445f88f51
macho199 <macho199@gmail.com>
2024-01-20 00:53:02 +0900

    add

--- 09-Practice-project/src/App.tsx
+++ 09-Practice-project/src/App.tsx
@@ -1,21 +1,13 @@
-import React, { useState } from "react";
-import tw, { TwStyle } from 'twin.macro'
-
-const styles: TwStyle[] = [tw`p-5 bg-red-500`, tw`bg-red-300`]
+import React, { useState } from 'react';
+import ProjectSidebar from './components/ProjectSidebar';
+import NewProject from './components/NewProject';
 
 const App = () => {
-  const [state, setState] = useState<number>(0);
-
-  const btnOnClickEventHandler = () => {
-    setState((prev) => {
-      return prev + 1;
-    });
-  };
-
   return (
-    <div>
-      <button css={[tw`bg-slate-500`, ...styles]} onClick={btnOnClickEventHandler}>{state}</button>
-    </div>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar />
+      <NewProject />
+    </main>
   );
 };
 
